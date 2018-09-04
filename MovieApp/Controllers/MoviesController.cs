275f0101f5f623e7510b65802d957197763c284b
vladitodorov <vladitodorov81@gmail.com>
2018-09-04 09:02:24 +0300

    Initial loading

---
+++ MovieApp/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿namespace MovieApp.Controllers
+{
+	using System.Web.Mvc;
+	using MovieApp.Models;
+
+	public class MoviesController : Controller
+	{
+		public ActionResult Random()
+		{
+			var movie = new Movie() { Name = "Shrek" };
+
+			return View(movie);
+		}
+
+		public ActionResult Edit(int id)
+		{
+			return Content("id=" + id);
+		}
+	}
+}
